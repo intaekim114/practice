bac699e3b17d08f065c236246029d4873d9e2af2
김인태 <114kit123@gmail.com>
2024-04-19 22:54:16 +0900

    연결리스트 추가

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // int 타입만 적재 가능
+        LinkedList<Integer> list = new LinkedList<>();
+
+        // 초기값 설정
+        LinkedList<Integer> list2 = new LinkedList<Integer>(Arrays.asList(1,2));
+    }
+}
