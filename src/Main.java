58c5296167f292a4460b97683168dbe581aa44d1
김인태 <114kit123@gmail.com>
2024-04-19 23:29:03 +0900

    연결리스트

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 import java.util.Arrays;
 import java.util.LinkedList;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         // int 타입만 적재 가능
